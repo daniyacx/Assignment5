e48c52c67cc27366200974dfcbee63b4e05be36e
aubak <aubakirovadania@gmail.com>
2023-05-22 00:40:02 +0600

    put() method

--- src/BST.java
+++ src/BST.java
@@ -1,7 +1,7 @@
 
 public class BST<K extends Comparable<K>,V> {
     private Node root;
-
+    private int size = 1;
     private class Node {
         private K key;
         private V val;
@@ -12,7 +12,20 @@ public class BST<K extends Comparable<K>,V> {
             this.val = val;
         }
     }
-    public void put(K key, V val) {
+    public void put(K key, V value) {
+        root = insertNode(root, key, value);
+    }
+    private Node insertNode(Node currentNode, K key, V value) {
+        if (currentNode == null) {
+            size++;
+            return new Node(key, value);
+        }
+        if (key.compareTo(currentNode.key) < 0) {
+            currentNode.left = insertNode(currentNode.left, key, value);
+        } else {
+            currentNode.right = insertNode(currentNode.right, key, value);
+        }
+        return currentNode;
     }
     public V get(K key) {
     }
